2589571ea01a9a0a74ac2de97c7a0c8ccc45ffc8
Harith93uom <lahkarunarathne@gmail.com>
2019-03-19 02:58:51 +0530

    first program

---
+++ TestClass.java
@@ -0,0 +1,9 @@
+class TestClass{
+	public static void main(String []args){
+		System.out.println("main method");
+	}
+
+	public static void main(String []args1, int args2){
+		System.out.println("main method with two arguments");
+	}
+}
